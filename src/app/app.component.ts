e4101e086cf85d8252b35592a947e45a287cda74
Devon Anderson <dvonanderson@gmail.com>
2018-10-17 13:34:50 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { DataService } from './data.service';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularPwaTest';
+
+  //boolean property for update -- default false
+  update: boolean = false;
+  quote: any;
+
+
+  //constructor that creates instance of SwUpdate
+  constructor(updates: SwUpdate, private data: DataService) {
+    //if updated is available we will subscribe via event.
+    updates.available.subscribe(event => {
+      
+      //if there is an update property gets changed to true.
+      // this.update = true;
+      updates.activateUpdate().then(() => {
+       document.location.reload() 
+      })
+    })
+  }
+
+  ngOnInit() {
+    this.data.getTheQuote().subscribe(res => {
+      this.quote = res;
+    })
+  }
 }
